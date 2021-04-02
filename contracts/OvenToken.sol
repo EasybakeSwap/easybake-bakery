86c112e57f599c6d7afc0e4dabc875c7da37d883
Uni <cryptounico@gmail.com>
2021-04-02 10:16:00 -0500

    Update OvenToken.sol

--- contracts/OvenToken.sol
+++ contracts/OvenToken.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.6.12;
 import "easybake-swap-lib/contracts/token/ERC20/ERC20.sol";
 
 // OvenToken with Governance.
-contract OvenToken is ERC20('Oven Token', 'Oven') {
+contract OvenToken is ERC20('Oven Token', 'OVEN') {
     /// @dev Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
